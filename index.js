dfe1825e6cb36e853a92077653d3066add002460
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-08 14:03:38 +0100

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const saturdayFun = function (arg = "roller-skate") {
+    return `This Saturday, I want to ${arg}!`
+}
+
+const mondayWork = function (arg = "go to the office") {
+    return `This Monday, I will ${arg}.`
+}
+
+const wrapAdjective = function (visualFlair = "*") {
+    
+    const inner = function (arg = "special") {
+        return `You are ${visualFlair + arg + visualFlair}!`;
+    }
+    
+    return inner;
+}
+
+console.log(wrapAdjective("%")())
